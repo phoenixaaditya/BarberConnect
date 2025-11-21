0ddbec63024ff4a976c6e2af48b4133067432798
Aditya <aadityanarware12@gmail.com>
2025-11-21 22:05:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,5 +35,5 @@ app.get(/.*/, (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(Backend server is running on http://localhost:${PORT})
+    console.log('Backend server is running on http://localhost:${PORT}')
 });
